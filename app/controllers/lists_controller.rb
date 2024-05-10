f7d7dfa3360d6dca478b9f8b81c74d7dd50d5d65
k <ktf.ahmynket5317@gmail.com>
2024-05-10 20:30:26 +0000

    once finished

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,17 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
   end
+
+#以下を追加
+def create
+  #1.&2. データを受けとり新規登録するためのインスタンス作成
+  list = List.new(list_params)
+  #3. データをデータベースに保存するためのsaveメソッド実行
+  list.save
+  #4. トップ画面へリダイレクト
+  redirect_to '/top'
+end
 
   def index
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get 'lists/index'
   get 'lists/show'
   get 'lists/edit'
+  
+  resources :lists
+  
   get '/top' => 'homes#top'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
